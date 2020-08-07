3530c4615b9c6fb8d3a00f68b490c41d2c92be46
Ch3did <phchedidk@gmail.com>
2020-08-07 13:38:08 -0300

    go

--- Filiações_Partidárias.py
+++ Filiações_Partidárias.py
@@ -1,8 +1,6 @@
 import urllib.request
 import os
 
-x = os.system("pwd")
-
 os.system("rm -r DadosEleitorais")
 os.system("mkdir DadosEleitorais")
 
@@ -10,7 +8,7 @@ os.system("mkdir DadosEleitorais")
 estados = ["ac", "al", "ap", "am" ,"ba" ,"ce", "df", "es", "go","ma", "mt" ,"ms", "mg" ,"pa" ,"pb" ,"pr", "pe", "pi", "rj", "rn", "rs", "ro", "rr", "sc", "sp", "se", "to"]
 
 #Lista de Partidos
-partidos = ["avante", "cidadania", "dc", "dem", "mdb","novo", "patriota","pcb", "pcdob" ,"pco", "pdt", "phs", "pl", "pmb", "pmn", "pode", "pp","ppl","pors","prp","prtb","psb","psc","psd","psdb","psl","psol","pstu","pt","ptb","ptc","pv","rede","republicanos","solidariedade"]
+partidos = ["phs","ppl","prp","avante","cidadania","dc","dem","mdb","novo","patriota","pcb","pcdob ","pco","pdt","pl","pmb","pmn","pode","pp","pros","prtb","psb","psc","psd","psdb","psl","psol","pstu","pt","ptb","ptc","pv","rede","republicanos","solidariedade","up"]
 
 part_lenth = len(partidos) 
 est_lenth = len(estados)
@@ -25,4 +23,4 @@ for cont in range (0,part_lenth):
             os.system("mv "+ nome + " DadosEleitorais/")
         except:
             print("Deu Erro")
-            pass    
+            pass   
